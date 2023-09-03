7e74991c74cf2aa4a97405fb1d87820f759d447d
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-09-02 22:29:57 -0300

    vetores

--- modulo3/Javascriptintrodução/aula3 - arrays e vetores/script.js
+++ modulo3/Javascriptintrodução/aula3 - arrays e vetores/script.js
@@ -2,3 +2,23 @@
 
 let arrays = ['string', 1, true];
 console.log(array);
+
+//forEach 
+array.forEach(function(item, index){console.log(item, index)})
+
+array.push([]);
+console.log(array);
+
+array.pop();
+console.log(array);
+
+let object = { string: 'string', number: 1, boolean: true,  array: ["array"], objectInterno: {
+ objectInterno: 'objeto interno'   
+} };
+console.log(object.boolean);
+
+var string = object.string;
+console.log(string);
+
+var srrayInterno = object.array;
+console.log(arrayInerno);
