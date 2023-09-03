78d860cfc88ecb26c6b4c5630692b4a651f90477
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-09-02 22:15:42 -0300

    arrays e vetores

---
+++ modulo3/Javascriptintrodução/aul2 - variaveis/script.js
@@ -0,0 +1,13 @@
+// tipos primitivos 
+
+//boolean 
+var VouF = false; 
+console.log(VouF);
+
+//number 
+var numeroQualqquer = 1; 
+console.log(typeof(numeroQualquer));
+
+//string 
+var nome = 'diana';
+console.log(typeof(nome));
---
+++ modulo3/Javascriptintrodução/aula3 - arrays e vetores/script.js
@@ -0,0 +1,4 @@
+// o que são vetores e arrya 
+
+let arrays = ['string', 1, true];
+console.log(array);
