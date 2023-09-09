c039e13a8d7a1d9fcf4e37c606c243629a88d196
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-09-08 22:45:24 -0300

    teste js

---
+++ modulo3 - dominando JavaScript/teste.js
@@ -0,0 +1,18 @@
+//var y = 0; 
+
+/*var x = 1; 
+
+var z = 2; 
+const preco = 2; 
+var total = desconto;
+
+preco = preco - desconto; */
+
+//função 
+
+function soma() {
+    console.log(a + b)
+    return a + b;
+}
+
+soma(3, 5);
