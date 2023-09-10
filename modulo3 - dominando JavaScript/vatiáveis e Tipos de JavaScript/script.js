e9e722be2e1512dce4cdc08efc5730ee6a488749
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-09-10 17:17:56 -0300

    var e let

---
+++ modulo3 - dominando JavaScript/vatiáveis e Tipos de JavaScript/script.js
@@ -0,0 +1,15 @@
+var firstName = "João";
+let lastName = "Souza";
+
+console.log(numberOne + 2);
+
+var numberOne;
+  
+var firstName = 'João';
+
+if(firstName === 'João') {
+    var fisrtName = 'Pedro';
+    let lastName = "Silva";
+}
+
+console.log(fisrtName, lastName);
