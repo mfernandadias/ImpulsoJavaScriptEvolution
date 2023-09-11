72d88b1f7c0056612c8b657332b43b8919519dc3
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-09-10 21:41:26 -0300

    palindromo - solução 01

--- modulo3 - dominando JavaScript/vatiáveis e Tipos de JavaScript/const.js
+++ modulo3 - dominando JavaScript/vatiáveis e Tipos de JavaScript/const.js
@@ -1,3 +1,4 @@
 const FIRST_NAME = "Stephany";
 
 console.log(FIRST_NAME);
+//estrutura de dados em JavaScript 
---
+++ modulo3 - dominando JavaScript/vatiáveis e Tipos de JavaScript/palindromo.js
@@ -0,0 +1,12 @@
+//Palíndromo 
+/* 
+frase ou palavra que se pode ler, indiferente, da esquerda para a direita ou vice-versa
+*/
+//SOLUÇÃO NÚMERO 2
+/*function verificaPalindromo(string){
+    if(!string) return "string inexistente"
+
+    return string.split(" ").reverse().join(" ") === string;
+}
+
+console.log(verificaPalindromo("ama")); */
