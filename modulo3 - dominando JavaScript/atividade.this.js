9560f7ef4404df0b9b9bfd2c651cfcd76481f0f6
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-09-11 22:28:00 -0300

    atividade 2 - this

---
+++ modulo3 - dominando JavaScript/atividade.this.js
@@ -0,0 +1,29 @@
+/* dada função calculada, utilize os métodos call e apply para modificar o valor de this. 
+crie seus próprios objetos para esta atividade 
+
+*/
+
+function calculaIdade(anos) {
+    return ` Daqui a ${anos} anos, ${this.nome} terá ${
+    this.idade + anos
+   } anos de idade.`;
+}
+
+const pessoa1 = {
+    nome: "Maria", 
+    idade: 30
+}
+
+const pessoa2 = {
+    nome: "Carla", 
+    idade: 26, 
+};
+
+const animal = {
+    nome: 'Fiona', 
+    idade: 5, 
+    raca: 'Pug',
+};
+
+//console.log(calculaIdade.call(animal, 30));
+console.log(calculaIdade.apply(animal, [7]));
